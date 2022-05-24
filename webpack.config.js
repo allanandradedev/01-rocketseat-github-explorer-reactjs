c0c15067d037f7951484268880759f571fcd7750
Allan Andrade <allanandrade.empreendimentos@gmail.com>
2022-05-24 18:53:41 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const reactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopment? 'development' : 'production',
+    devtool: isDevelopment? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        hot: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    plugins: [
+        isDevelopment && new reactRefreshWebpackPlugin(),
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname,'public', 'index.html')
+        })
+    ].filter(Boolean),
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ],
+    }
+}
